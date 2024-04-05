2b6588f15b5a5ebebdabaa31f9c38479ec36a864
liviatanaka <liviatanaka19@gmail.com>
2024-04-05 17:06:15 -0300

    read, update and delete

--- src/main/java/insper/collie/squad/SquadController.java
+++ src/main/java/insper/collie/squad/SquadController.java
@@ -1,7 +1,10 @@
 package insper.collie.squad;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,27 @@ public interface SquadController {
     @PostMapping("/squad")
     public ResponseEntity<SquadInfo> create(
         @RequestBody(required = true) SquadInfo in
+    );
+
+    
+    @GetMapping("/squad/{id}")
+    public ResponseEntity<SquadInfo> getSquad(
+        @PathVariable(required = true) String id
+    );
+
+    @GetMapping("/squad")
+    public ResponseEntity<List<SquadInfo>> getAllSquads();
+
+
+    @PutMapping("/squad/{id}")
+    public ResponseEntity<SquadInfo> updateSquad(
+        @PathVariable(required = true) String id,
+        @RequestBody(required = true) SquadInfo in
+    );
+
+    @DeleteMapping("/squad/{id}")
+    public ResponseEntity<SquadInfo> deleteSquad(
+        @PathVariable(required = true) String id
     );
 
 }
